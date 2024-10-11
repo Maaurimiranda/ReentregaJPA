90938b5f21566340a7e771d807c18cb60b4d8aff
maaurimiranda <mirandamauri242@gmail.com>
2024-10-11 18:22:30 -0300

    first commit

---
+++ src/main/java/org/example/entidades/Domicilio.java
@@ -0,0 +1,31 @@
+package org.example.entidades;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+@Entity
+@Table(name = "Domicilio")
+public class Domicilio implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombreCalle;
+    private int numero;
+
+    //relacion con cliente 1-1
+    @OneToOne(mappedBy = "domicilio")
+    private Cliente cliente;
+
+    public Domicilio(String nombreCalle, int numero) {
+        this.nombreCalle = nombreCalle;
+        this.numero = numero;
+    }
+}
